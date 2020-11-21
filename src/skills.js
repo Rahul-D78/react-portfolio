ea0c9a992d2c70f445f430fc3bfe206518976181
Rahul kumar <rahulparida933@gamil.com>
2020-11-21 16:26:07 +0530

    card component added

---
+++ src/skills.js
@@ -0,0 +1,29 @@
+const skills = [
+    {
+      id : 1,
+      name : "Javascript",
+      lavel : 80
+    },
+    {
+      id : 2 ,
+      name : "Node.js",
+      level : 70 
+    },
+    {
+        id : 3,
+        name : "devops",
+        level : 70
+    },
+    {
+        id : 4,
+        name : "SQL",
+        lavel : 70
+    },
+    {
+        id: 5,
+        name : "React",
+        lavel : 70
+    }
+]
+
+export default skills;
